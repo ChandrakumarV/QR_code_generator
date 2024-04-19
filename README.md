b5df26fb3c78c4efe6f3b1e6b8e154916c735204
ChandrakumarV <chandrakumar252004@gmail.com>
2024-04-19 23:15:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# QR code generator
+
+- Clone this repository and run this cmds
+> npm i
+
+> node index.js
+- Enter a text in console to generate a QR_code
+- see a root dir qr_code.png was generated 
---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {
+        "message" : "Please enter your text : ",
+        "name":"text"
+    }
+  ])
+  .then((answers) => {
+    var qr_png = qr.image(answers.text);
+    qr_png.pipe(fs.createWriteStream('qr_code.png'));
+  })
